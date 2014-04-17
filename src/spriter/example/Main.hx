a8974e0aeace90ae8d96a2eae592a4cf5ee8773c
loudoweb <loudoweb@gmail.com>
2014-04-17 10:49:43 +0200

    update with last version of SpriterHaxeEngine (v0.9)

--- src/spriter/example/Main.hx
+++ src/spriter/example/Main.hx
@@ -1,13 +1,15 @@
-package spriter.example;
+package;
 
 import flash.display.Bitmap;
 import flash.display.BitmapData;
 import flash.display.PixelSnapping;
 import flash.display.Sprite;
 import flash.events.Event;
+import flash.events.FocusEvent;
 import flash.events.KeyboardEvent;
 import flash.events.MouseEvent;
 import flash.Lib;
+import flash.text.TextField;
 import flash.ui.Keyboard;
 import openfl.Assets;
 import openfl.display.FPS;
@@ -17,12 +19,15 @@ import spriter.engine.SpriterEngine;
 import spriter.library.BitmapLibrary;
 import spriter.library.SpriterLibrary;
 import spriter.library.TilelayerLibrary;
+import spriter.macros.SpriterMacros;
 
 /**
  * To test : 
  * Press INSERT key to add an entity
  * Press A key to change animation
- * Press SPACE key to change character mapping
+ * Press M key to change character mapping
+ * Press DEL key to remove
+ * Press SPACE to pause
  * @author Loudo
  */
 
@@ -32,15 +37,16 @@ class Main extends Sprite
 
 	/* ENTRY POINT */
 	
-	public var engine:SpriterEngine;
-	public var scml:ScmlObject;
+	var engine:SpriterEngine;
+	var scml:ScmlObject;
 	
-	public var len:Int = 1;//number of spriter entity
+	var len:Int = 1;//number of spriter entity
+	var line:Sprite;
 	
-	public var currentAnim:Int = 0;
-	public var currentCharMap:Int = -1;
-	public var anims:Array<String> = ['normal', 'instant', 'bone'];//TODO get automatically
-	public var charMaps:Array<String> = ['gold', 'circle']; //TODO get automatically
+	var currentAnim:Int = 0;
+	var currentCharMap:Int = -1;
+	var anims:Array<String> = ['instant','bone','normal','alpha'];//TODO get automatically
+	var charMaps:Array<String> = ['gold', 'circle']; //TODO get automatically
 	
 	function resize(e) 
 	{
@@ -53,67 +59,32 @@ class Main extends Sprite
 		if (inited) return;
 		inited = true;
 		
-		
+		//stats
+		#if (!stats || flash)
 		var fps:FPS = new FPS();
 		addChild(fps);
+		#end
 		
-		/*
-		 * BLOCK 1 : Simple bitmap library
-		 */
-		/*
-		var spriterRoot:Sprite = new Sprite();
+		SpriterMacros.texturePackerChecker('assets/ugly/ugly.xml');
 		
-		var lib:SpriterLibrary = new SpriterLibrary('assets/ugly/');
+		addControls();
 		
-		engine = new SpriterEngine(Assets.getText('assets/ugly/ugly_b6_1.scml'), lib, spriterRoot );
-
-		for (i in 0...len) {
-			engine.addEntity('lib_' + Std.int(i+1),  75 * (i % 10),  100 * (Std.int(i / 10) % 6));
-		}
-		*/
-		/*
-		 * END BLOCK 1
-		 */
-		/*
-		 * BLOCK 2 : tilelayer library
-		 */
+		createTilelayer(null);
 		
-		var spriterRoot:Sprite = new Sprite();
-		
-		var lib:TilelayerLibrary = new TilelayerLibrary('assets/ugly/ugly.xml' , 'assets/ugly/ugly.png');
-		engine = new SpriterEngine(Assets.getText('assets/ugly/ugly_b6_1.scml'), lib, spriterRoot );
-		for (i in 0...len) {
-			engine.addEntity('lib_' + Std.int(i+1),  75 * (i % 10),  100 * (Std.int(i / 10) % 6));
-		}
-		
-		/*
-		 * END BLOCK 2
-		 */
-		/*
-		 * BLOCK 3 : use BitmapLibrary
-		 */
-		/*
-		var canvas:BitmapData = new BitmapData(800, 480);
-		var spriterRoot:Bitmap = new Bitmap(canvas, PixelSnapping.AUTO, true);
-		
-		var lib:BitmapLibrary = new BitmapLibrary('assets/ugly/', canvas);
-		
-		engine = new SpriterEngine(Assets.getText('assets/ugly/ugly_b6_1.scml'), lib, null );
-		
-		for (i in 0...len) {
-			engine.addEntity('lib_' + Std.int(i+1),  75 * (i % 10),  100 * (Std.int(i / 10) % 6));
-		}
-		*/
-		/*
-		 * END BLOCK 3
-		 */
-		
-		
-		addChild(spriterRoot);
-		addEventListener(Event.ENTER_FRAME, onEnterFrame);
 		stage.addEventListener(KeyboardEvent.KEY_DOWN, onKey);
-		
+		stage.addEventListener(Event.ENTER_FRAME, onEnterFrame);
+		// Focus gained/lost monitoring
+		#if desktop
+		stage.addEventListener(FocusEvent.FOCUS_OUT, onFocusLost);
+		stage.addEventListener(FocusEvent.FOCUS_IN, onFocus);
+		#else
+		stage.addEventListener(Event.DEACTIVATE, onFocusLost);
+		stage.addEventListener(Event.ACTIVATE, onFocus);
+		#end
 	}
+	
+	/* Spriter engine handler stuff */
+	
 	private function onKey(e:KeyboardEvent):Void
 	{
 		switch(e.keyCode) {
@@ -136,15 +107,20 @@ class Main extends Sprite
 					currentAnim = 0;
 					engine.getEntityAt(0).playAnim(anims[currentAnim]);
 				}
+			case Keyboard.Z:
+				/*
+				 * Play a stack o animations with callback :
+				 */
+				engine.getEntityAt(0).playAnimsStack(anims, handlePause);
 			case Keyboard.INSERT:
 				len++;
 				/*
 				 * Add new entity
 				 */
-				engine.addEntity('lib_' + len,  100 * ((len - 1) % 10),   100 * (Std.int((len - 1) / 10) % 6));
+				engine.addEntity('lib_' + len,  100 * ((len - 1) % 8),   100 * (Std.int((len - 1) / 8) % 6));
 			case Keyboard.DELETE:
 				/*
-				 * Remove new entity
+				 * Remove entity
 				 */
 				engine.removeEntityAt(0);
 			case Keyboard.S:
@@ -169,18 +145,23 @@ class Main extends Sprite
 		}
 	}
 	
-	private function handleNextAnim(s:Spriter, anim:String):Void
+	private function handleNextAnim(s:Spriter, entity:String, anim:String):Void
 	{
+		trace(s.spriterName, entity, anim);
 		if (currentAnim + 1 >= anims.length)
 			currentAnim = -1;
-		engine.getEntityAt(0).playAnim(anims[++currentAnim]);
+		s.playAnim(anims[++currentAnim], handleNextAnim);
+	}
+	
+	private function handlePause(s:Spriter, entity:String, anim:String):Void
+	{
+		s.paused = true;
 	}
 	
 	private function onEnterFrame(e:Event):Void
 	{
 		engine.update();
 	}
-	
 	private function onFocusLost(e:Event):Void
 	{
 		if(!engine.paused){
@@ -194,6 +175,120 @@ class Main extends Sprite
 			stage.frameRate = engine.framerate;
 			engine.unpause();
 		}
+	}
+	
+	/* Creation stuff */
+	function createBitmap(e:MouseEvent):Void
+	{
+		
+		if (engine != null)
+			engine.destroy();
+		len = 1;
+		line.x = 470;
+		/*
+		 * Simple bitmap library
+		 */
+		
+		var spriterRoot:Sprite = new Sprite();
+		spriterRoot.y = 30;
+		var lib:SpriterLibrary = new SpriterLibrary('assets/ugly/');
+		
+		engine = new SpriterEngine(Assets.getText('assets/ugly/ugly_b6_1.scml'), lib, spriterRoot );
+
+		for (i in 0...len) {
+			engine.addEntity('lib_' + Std.int(i+1),  75 * (i % 8),  100 * (Std.int(i / 8) % 6));
+		}
+		addChild(spriterRoot);
+	}
+	function createTilelayer(e:MouseEvent):Void
+	{
+		if (engine != null)
+			engine.destroy();
+		len = 1;
+		line.x = 560;
+		/*
+		 * tilelayer library
+		 */
+		
+		var spriterRoot:Sprite = new Sprite();
+		spriterRoot.y = 30;
+		var lib:TilelayerLibrary = new TilelayerLibrary('assets/ugly/ugly.xml' , 'assets/ugly/ugly.png');
+		engine = new SpriterEngine(Assets.getText('assets/ugly/ugly_b6_1.scml'), lib, spriterRoot );
+		
+		for (i in 0...len) {
+			engine.addEntity('lib_' + Std.int(i+1),  75 * (i % 8),  100 * (Std.int(i / 8) % 6));
+		}
+		addChild(spriterRoot);
+	}
+	function createCopypixels(e:MouseEvent):Void
+	{
+		if (engine != null)
+			engine.destroy();
+		len = 1;
+		line.x = 630;
+		/*
+		 *  use BitmapLibrary
+		 */
+		
+		var canvas:BitmapData = new BitmapData(900, 470);
+		var spriterRoot:Bitmap = new Bitmap(canvas, PixelSnapping.AUTO, true);
+		spriterRoot.y = 30;
+		var lib:BitmapLibrary = new BitmapLibrary('assets/ugly/', canvas);
+		
+		engine = new SpriterEngine(Assets.getText('assets/ugly/ugly_b6_1.scml'), lib, null );
+		
+		for (i in 0...len) {
+			engine.addEntity('lib_' + Std.int(i+1),  75 * (i % 8),  100 * (Std.int(i / 8) % 6));
+		}
+		addChild(spriterRoot);
+	}
+	
+	function addControls():Void
+	{
+		var txt1:TextField = new TextField();
+		txt1.text = 'Simple Bitmap';
+		txt1.height = 20;
+		txt1.width = 85;
+		var btn1:Sprite = new Sprite();
+		btn1.buttonMode = true;
+		btn1.mouseChildren = false;
+		btn1.addEventListener(MouseEvent.CLICK, createBitmap);
+		btn1.addChild(txt1);
+		btn1.x = 470;
+		btn1.y = 10;
+		addChild(btn1);
+		//2
+		var txt2:TextField = new TextField();
+		txt2.text = 'Tilelayer';
+		txt2.height = 20;
+		txt2.width = 85;
+		var btn2:Sprite = new Sprite();
+		btn2.buttonMode = true;
+		btn2.mouseChildren = false;
+		btn2.addEventListener(MouseEvent.CLICK, createTilelayer);
+		btn2.addChild(txt2);
+		btn2.x = 560;
+		btn2.y = 10;
+		addChild(btn2);
+		//3
+		var txt3:TextField = new TextField();
+		txt3.text = 'Bitmap copypixels';
+		txt3.height = 20;
+		txt3.width = 100;
+		var btn3:Sprite = new Sprite();
+		btn3.buttonMode = true;
+		btn3.mouseChildren = false;
+		btn3.addEventListener(MouseEvent.CLICK, createCopypixels);
+		btn3.addChild(txt3);
+		btn3.x = 650;
+		btn3.y = 10;
+		addChild(btn3);
+		//line
+		line = new Sprite();
+		line.graphics.beginFill(0x000000);
+		line.graphics.drawRect(0, 0, 50, 4);
+		line.graphics.endFill();
+		addChild(line);
 	}
 
 	/* SETUP */
